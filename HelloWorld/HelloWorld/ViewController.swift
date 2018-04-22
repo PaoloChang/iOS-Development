da8164e1a845b5a382b40429be03076630d1da0f
Paolo Chang <chanhwan.chang@gmail.com>
2018-04-21 22:47:46 -0400

    Hello World In Swift 4
    First iOS application using xcode 9

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Chanhwan on 2018-04-21.
+//  Copyright © 2018 Chanhwan_Udemy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
