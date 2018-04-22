d6e0fa2a154d52c537b41e337f34645899d985b9
Paolo Chang <chanhwan.chang@gmail.com>
2018-04-22 00:33:12 -0400

    I Am Rich Application
    iOS Interface Builder with an Image

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Chanhwan on 2018-04-21.
+//  Copyright © 2018 Chanhwan_Udemy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
